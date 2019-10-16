ff3a9feac33af3570238e8e25b5a1325b5150e6f
Petrol95 <petrol195@yandex.ru>
2019-10-16 22:41:15 +0300

    created class JdbcTemplateDiskDao

--- connection/src/main/java/com/geekbrains/controllers/MainController.java
+++ connection/src/main/java/com/geekbrains/controllers/MainController.java
@@ -1,13 +1,24 @@
 package com.geekbrains.controllers;
 
+import com.geekbrains.dao.JdbcTemplateDiskDao;
+import com.geekbrains.entities.Disk;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    JdbcTemplateDiskDao jdbcTemplateDiskDao;
+
     @RequestMapping("/")
     public String showHomePage() {
+        Disk disk = new Disk();
+        disk.setId(0L);
+        disk.setTitle("Hello");
+        jdbcTemplateDiskDao.insert(disk);
+        System.out.println(jdbcTemplateDiskDao.findTitleById(1L));
         return "index";
     }
 }
---
+++ connection/src/main/java/com/geekbrains/dao/JdbcTemplateDiskDao.java
@@ -0,0 +1,36 @@
+package com.geekbrains.dao;
+
+import com.geekbrains.entities.Disk;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class JdbcTemplateDiskDao implements DiskDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcTemplateDiskDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void insert(Disk disk) {
+        if (disk.getId() > 0) {
+            // update
+            String sql = "UPDATE disks SET title = ? WHERE id = ?;";
+            jdbcTemplate.update(sql, disk.getTitle(), disk.getId());
+        } else {
+            // insert
+            String sql = "INSERT INTO disks (title) VALUES (?)";
+            jdbcTemplate.update(sql, disk.getTitle());
+        }
+    }
+
+    @Override
+    public String findTitleById(Long id) {
+        return jdbcTemplate.queryForObject("SELECT title FROM disks WHERE id = ?;", new Object[]{id}, String.class);
+    }
+}
