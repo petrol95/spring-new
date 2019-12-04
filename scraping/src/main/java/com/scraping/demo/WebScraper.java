a2eb0b8581176ec89fef340fcccc6f6cc31d1d79
Petrol95 <petrol195@yandex.ru>
2019-12-04 23:27:42 +0300

    added scraping eldorado.ru

--- scraping/src/main/java/com/scraping/demo/WebScraper.java
+++ scraping/src/main/java/com/scraping/demo/WebScraper.java
@@ -2,12 +2,11 @@ package com.scraping.demo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.HttpHeader;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomNode;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -18,7 +17,7 @@ import java.util.List;
 
 public class WebScraper {
 
-    public static void main(String[] args) throws FailingHttpStatusCodeException, IOException {
+    public static void main(String[] args) throws Exception {
 
         // scraping Indian films from wikipedia
 //        getIndianFilms();
@@ -29,42 +28,60 @@ public class WebScraper {
         }
 
     private static void getItemInfo() throws IOException {
-        String searchQuery = "iphone xs";
-        String baseUrl = "https://newyork.craigslist.org/";
 
-        WebClient client = new WebClient();
-        client.getOptions().setCssEnabled(false);
-        client.getOptions().setJavaScriptEnabled(false);
-        client.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        WebClient webClient = new WebClient(BrowserVersion.BEST_SUPPORTED);
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(false);
+        HtmlPage page = webClient.getPage("https://www.eldorado.ru/search/catalog.php?q=iphone%20xs");
+        System.out.println(page.asXml());
 
-        // https://newyork.craigslist.org/search/sss?query=iphone+xs
-        String searchUrl = baseUrl + "search/sss?query=" + URLEncoder.encode(searchQuery, "UTF-8");
-        HtmlPage page = client.getPage(searchUrl);
+        HtmlInput inputZona = (HtmlInput) page.getElementById("search_price_min");
+        System.out.println(inputZona.asXml());
 
-        List<HtmlElement> items = page.getByXPath("//li[@class='result-row']");
-
-        if (items.isEmpty()) {
-            System.out.println("No items found !");
-        } else {
-            for (HtmlElement htmlItem : items) {
-                HtmlAnchor itemAnchor = htmlItem.getFirstByXPath(".//p[@class='result-info']/a");
-                HtmlElement spanPrice = htmlItem.getFirstByXPath(".//a/span[@class='result-price']");
-
-                // It is possible that an item doesn't have any price, we set the price to 0.0 in this case
-                String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText();
-
-                Item item = new Item();
-                item.setTitle(itemAnchor.asText());
-                item.setUrl(baseUrl + itemAnchor.getHrefAttribute());
-
-                item.setPrice(new BigDecimal(itemPrice.replace("$", "")));
-
-                ObjectMapper mapper = new ObjectMapper();
-                String jsonString = mapper.writeValueAsString(item);
-
-                System.out.println(jsonString);
-            }
-        }
+//        String searchQuery = "iphone xs";
+//        String baseUrl = "https://www.eldorado.ru/";
+//
+//        WebClient client = new WebClient();
+//        client.getOptions().setCssEnabled(false);
+//        client.getOptions().setJavaScriptEnabled(false);
+//        client.addRequestHeader(HttpHeader.ACCEPT_ENCODING, "windows-1251");
+//        client.addRequestHeader(HttpHeader.ACCEPT_LANGUAGE, "ru");
+//        client.getOptions().setThrowExceptionOnFailingStatusCode(false);
+//        client.getOptions().setThrowExceptionOnScriptError(false);
+//        client.getOptions().setRedirectEnabled(true);
+//
+//        // https://www.eldorado.ru/search/catalog.php?q=iphone%20xs
+//        String searchUrl = baseUrl + "search/catalog.php?q=" + URLEncoder.encode(searchQuery, "UTF-8");
+//        System.out.println(searchUrl);
+//        HtmlPage page = client.getPage(searchUrl);
+//
+//        List<HtmlElement> items = page.getByXPath("//div[@class='item']");
+//
+//        System.out.println("==========================");
+//        System.out.println(items);
+//
+//        if (items.isEmpty()) {
+//            System.out.println("No items found !");
+//        } else {
+//            for (HtmlElement htmlItem : items) {
+//                HtmlAnchor itemAnchor = htmlItem.getFirstByXPath(".//div[@class='itemTitle']/a");
+//                HtmlElement spanPrice = htmlItem.getFirstByXPath(".//div/span[@class='discountPrice.itemPrice']");
+//
+//                // It is possible that an item doesn't have any price, we set the price to 0.0 in this case
+//                String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText();
+//
+//                Item item = new Item();
+//                item.setTitle(itemAnchor.asText());
+//                item.setUrl(baseUrl + itemAnchor.getHrefAttribute());
+//
+//                item.setPrice(new BigDecimal(itemPrice.replace("$", "")));
+//
+//                ObjectMapper mapper = new ObjectMapper();
+//                String jsonString = mapper.writeValueAsString(item);
+//
+//                System.out.println(jsonString);
+//            }
+//        }
     }
 
     private static void getIndianFilms() throws IOException {
